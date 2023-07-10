597c4fa2e4bdcb4090ff5b9b57de5142fd989524
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-07-10 11:01:36 +0530

    init-packages

---
+++ src/components/VideoComponent.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Project Name   : react-video
+ * Project Author : mindula dilthushan
+ * Author Email   : minduladilthushan1@gmail.com
+ *
+ */
+
+const VideoComponent = () => {
+  return (
+    <div>
+      <h1> Hello React Video </h1>
+    </div>
+  )
+}
+
+export default VideoComponent;
